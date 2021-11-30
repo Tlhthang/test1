7e6f5574baa92130a07d69d8be7ed87188ed3bf8
Thang Tran <tranlehoangthang.hanoi@gmail.com>
2021-12-01 01:55:41 +0700

    Commit2: Change into real example

--- Encapsulation - Protected Variable.py
+++ Encapsulation - Protected Variable.py
@@ -1,1 +1,17 @@
-print("Hello World")
+class Human:
+    def __init__(self):
+        #set default = high, not allow user enter
+        self._intelligence = 'high'
+
+class Vietnamese(Human):
+    def __init__(self, name):
+        self.name = name
+        Human.__init__(self)
+
+    def showinfo(self):
+        print('Name: ', self.name,'IQ level: ', self._intelligence)
+
+nguoi = Vietnamese('Ho Chi Minh')
+nguoi.showinfo()
+#not work 'caused protected:
+#print("Iq: ", self._intelligence)
